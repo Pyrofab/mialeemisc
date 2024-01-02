3f6e6eb268a1642b8a370d76251190290810c545
Pyrofab <pyrofab@ladysnake.org>
2024-01-02 23:09:03 +0100

    Partially update to 1.20.1

--- src/main/java/xyz/amymialee/mialeemisc/mixin/client/CreativeInventoryScreenMixin.java
+++ src/main/java/xyz/amymialee/mialeemisc/mixin/client/CreativeInventoryScreenMixin.java
@@ -14,7 +14,6 @@ import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import xyz.amymialee.mialeemisc.itemgroup.MialeeItemGroup;
 
 @Mixin(CreativeInventoryScreen.class)
 public abstract class CreativeInventoryScreenMixin extends AbstractInventoryScreen<CreativeInventoryScreen.CreativeScreenHandler> {
